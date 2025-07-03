679258f3297991db2b67978b3b17111aaba9e9ab
gh4ni404 <gania828@gmail.com>
2025-07-04 06:18:38 +0800

    feat: Implement basic complete profile screen
    - Add `CompleteProfileFragment` with layout inflation and view binding.
    - Update `fragment_complete_profile.xml` to use placeholder drawables for profile image.
    - Add logout functionality to `MainActivity`.
    - Fix constraint in `fragment_register.xml`.

--- app/src/main/java/com/smkn8bone/jejakpklskadel/MainActivity.kt
+++ app/src/main/java/com/smkn8bone/jejakpklskadel/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.smkn8bone.jejakpklskadel
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.smkn8bone.jejakpklskadel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogout.setOnClickListener {
+            auth.signOut()
+            startActivity(Intent(this, AuthActivity::class.java))
         }
     }
+
 }
--- app/src/main/java/com/smkn8bone/jejakpklskadel/ui/completeprofile/CompleteProfileFragment.kt
+++ app/src/main/java/com/smkn8bone/jejakpklskadel/ui/completeprofile/CompleteProfileFragment.kt
@@ -1,6 +1,29 @@
 package com.smkn8bone.jejakpklskadel.ui.completeprofile
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.smkn8bone.jejakpklskadel.databinding.FragmentCompleteProfileBinding
 
 class CompleteProfileFragment : Fragment() {
+    private var _binding: FragmentCompleteProfileBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: CompleteProfileViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCompleteProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
