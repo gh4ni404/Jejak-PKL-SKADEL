0f611c4fd4921c9781b4a782bf419d75b3e96452
gh4ni404 <gania828@gmail.com>
2025-07-04 12:56:46 +0800

    Refactor: Use LoadingOverlayView in LoginFragment
    - Replace `ProgressBar` with `LoadingOverlayView` for a more integrated loading experience.
    - Update `LoginViewModel.LoginState` observers to show/hide the `LoadingOverlayView` accordingly.

--- app/src/main/java/com/smkn8bone/jejakpklskadel/ui/login/LoginFragment.kt
+++ app/src/main/java/com/smkn8bone/jejakpklskadel/ui/login/LoginFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.smkn8bone.jejakpklskadel.R
 import com.smkn8bone.jejakpklskadel.databinding.FragmentLoginBinding
@@ -39,21 +38,23 @@ class LoginFragment : Fragment() {
             viewModel.login(email, password)
         }
 
-        viewModel.loginState.observe(viewLifecycleOwner, Observer { state ->
+        viewModel.loginState.observe(viewLifecycleOwner) { state ->
             when (state) {
                 is LoginViewModel.LoginState.Loading -> {
-                    binding.progressBar.visibility = View.VISIBLE
+                    binding.loadingOverlay.show()
                 }
+
                 is LoginViewModel.LoginState.Success -> {
-                    binding.progressBar.visibility = View.GONE
+                    binding.loadingOverlay.hide()
 //                    findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
                 }
+
                 is LoginViewModel.LoginState.Error -> {
-                    binding.progressBar.visibility = View.GONE
+                    binding.loadingOverlay.hide()
                     Toast.makeText(requireContext(), state.message, Toast.LENGTH_SHORT).show()
                 }
             }
-        })
+        }
     }
 
     override fun onDestroyView() {
