6889a6752005f3fb41cff2de72a04c8a6182614b
gh4ni404 <gania828@gmail.com>
2025-07-04 23:49:33 +0800

    Refactor: Remove unused import in FileUtils.kt
    - Remove `android.provider.OpenableColumns` import as it is no longer used.

--- app/src/main/java/com/smkn8bone/jejakpklskadel/utils/FileUtils.kt
+++ app/src/main/java/com/smkn8bone/jejakpklskadel/utils/FileUtils.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
-import android.provider.OpenableColumns
 import java.io.File
 import java.io.FileOutputStream
 
