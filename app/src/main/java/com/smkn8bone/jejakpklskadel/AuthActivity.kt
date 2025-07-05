f13b8eb3c7cf2b317f89df628eb2bf473cbece14
gh4ni404 <gania828@gmail.com>
2025-07-06 00:24:21 +0800

    feat: Add redirection to CompleteProfileFragment in AuthActivity
    - Retrieve a boolean extra `redirectToCompleteProfile` from the intent.
    - If true, find the `NavHostFragment` and its `NavController`.
    - Navigate to `CompleteProfileFragment` using the `NavController`.

--- app/src/main/java/com/smkn8bone/jejakpklskadel/AuthActivity.kt
+++ app/src/main/java/com/smkn8bone/jejakpklskadel/AuthActivity.kt
@@ -1,11 +1,20 @@
 package com.smkn8bone.jejakpklskadel
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        val shouldRedirect = intent.getBooleanExtra("redirectToCompleteProfile", false)
+        if (shouldRedirect) {
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.auth_nav_host_fragment) as? NavHostFragment
+
+            val navController = navHostFragment?.navController
+            navController?.navigate(R.id.completeProfileFragment)
+        }
     }
 }
