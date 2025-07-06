4c61da54b77896e2e6873c83acd452e7b49985da
gh4ni404 <gania828@gmail.com>
2025-07-06 20:54:30 +0800

    Refactor: Remove edge-to-edge display and logout functionality
    - Remove the call to `enableEdgeToEdge()` in `MainActivity`.
    - Remove the `btnLogout` OnClickListener that handled user logout and navigation to `AuthActivity`.
    - Comment out the `setContentView(R.layout.activity_main)` line as the view is now set via data binding.

--- app/src/main/java/com/smkn8bone/jejakpklskadel/MainActivity.kt
+++ app/src/main/java/com/smkn8bone/jejakpklskadel/MainActivity.kt
@@ -1,28 +1,19 @@
 package com.smkn8bone.jejakpklskadel
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.firebase.auth.FirebaseAuth
 import com.smkn8bone.jejakpklskadel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+//    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnLogout.setOnClickListener {
-            auth.signOut()
-            startActivity(Intent(this, AuthActivity::class.java))
-        }
     }
 
 }
