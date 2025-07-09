8fb93d89b016067803c318bb8f0c774fb64e0fde
gh4ni404 <gania828@gmail.com>
2025-07-09 21:30:49 +0800

    feat: Implement BottomNavigationView with Navigation Component
    - Initialize `BottomNavigationView` in `MainActivity`.
    - Find `NavHostFragment` and get its `NavController`.
    - Set up `BottomNavigationView` with the `NavController` using `setupWithNavController`.
    - Add a function `getBottomNavView` to access the `BottomNavigationView` instance.

--- app/src/main/java/com/smkn8bone/jejakpklskadel/MainActivity.kt
+++ app/src/main/java/com/smkn8bone/jejakpklskadel/MainActivity.kt
@@ -2,11 +2,15 @@ package com.smkn8bone.jejakpklskadel
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.smkn8bone.jejakpklskadel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 //    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
@@ -14,6 +18,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        bottomNavView = binding.bottomNav
+
+        binding.bottomNav.setupWithNavController(navController)
+//        val bottomNav = binding.bottomNav
+//        NavigationUI.setupWithNavController(bottomNav, navController)
+
+    }
+
+    fun getBottomNavView(): BottomNavigationView {
+        return binding.bottomNav
     }
 
 }
