8500fdd11c95489ab43f574c57bffda27d5ca15b
gh4ni404 <gania828@gmail.com>
2025-07-10 02:43:06 +0800

    Refactor: Remove unused `getFileNameFromUri` method in `FileUtils`
    - Delete the commented-out `getFileNameFromUri` function.
    - Change the compression quality in `compressImage` from 80 to 60.

--- app/src/main/java/com/smkn8bone/jejakpklskadel/utils/FileUtils.kt
+++ app/src/main/java/com/smkn8bone/jejakpklskadel/utils/FileUtils.kt
@@ -8,37 +8,12 @@ import java.io.File
 import java.io.FileOutputStream
 
 object FileUtils {
-//    fun getFileNameFromUri(context: Context, uri: Uri): String? {
-//        var result: String? = null
-//        if (uri.scheme == "content") {
-//            val cursor = context.contentResolver.query(uri, null, null, null, null)
-//            try {
-//                cursor?.let {
-//                    if (it.moveToFirst()) {
-//                        result = it.getString(it.getColumnIndexOrThrow(OpenableColumns.DISPLAY_NAME))
-//                    }
-//                }
-//            } finally {
-//                cursor?.close()
-//
-//            }
-//        }
-//        if (result == null) {
-//            result = uri.path
-//            val cut = result?.lastIndexOf('/')
-//            if (cut != null && cut != -1) {
-//                result = result?.substring(cut + 1)
-//            }
-//        }
-//        return result
-//    }
-
     fun compressImage(context: Context, uri: Uri): File {
         val inputStream = context.contentResolver.openInputStream(uri)
         val bitmap = BitmapFactory.decodeStream(inputStream)
         val compressedFile = File.createTempFile("compressed_", ".jpg", context.cacheDir)
         val outputStream = FileOutputStream(compressedFile)
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 80, outputStream)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 60, outputStream)
         outputStream.flush()
         outputStream.close()
         return compressedFile
